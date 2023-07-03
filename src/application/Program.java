2b0a65d1a5fd2783e3a012e87ca274f298072f19
GabrielPertile <gabriel-pertile@hotmail.com>
2023-07-03 14:29:17 -0300

    Insert impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -27,14 +28,21 @@ public class Program {
 			System.out.println(obj);
 		}
 
-		System.out.println("=== TEST : seller findAll ===");
+		System.out.println("=== TEST3 : seller findAll ===");
 
-		List<Seller> listAll = sellerDao.findAll();
+		list = sellerDao.findAll();
 		
-		for (Seller obj : listAll) {
+		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 
+		System.out.println("=== TEST4 : seller insert===");
+
+		Seller newSeller = new Seller(null, "Lucas", "lucas@gmail.com", new Date(), 10000.00, dep);
+	
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+	
 	}
 
 }
