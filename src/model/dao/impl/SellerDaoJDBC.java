a631ba55461ae4137b992fd481825aaadec6cb4d
GabrielPertile <gabriel-pertile@hotmail.com>
2023-07-03 13:57:44 -0300

    findByDepartment impl

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -67,6 +70,51 @@ public class SellerDaoJDBC implements SellerDao {
 		}
 	}
 
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.id " + "WHERE DepartmentID = ? " + "ORDER BY Name");
+			st.setInt(1, department.getId());
+
+			rs = st.executeQuery();
+
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+
+			while (rs.next()) {
+
+//				Verifica se já existe a o departamento no map
+				Department dep = map.get(rs.getInt("DepartmentId"));
+
+//				Se não existir, instancia um novo Department e adiciona no map
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+
+				list.add(instantiateSeller(rs, dep));
+			}
+
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
 	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
 		Seller seller = new Seller(rs.getInt("Id"), rs.getString("Name"), rs.getString("Email"),
 				rs.getDate("BirthDate"), rs.getDouble("BaseSalary"), dep);
@@ -78,11 +126,4 @@ public class SellerDaoJDBC implements SellerDao {
 		Department dep = new Department(rs.getInt("DepartmentId"), rs.getString("DepName"));
 		return dep;
 	}
-
-	@Override
-	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
