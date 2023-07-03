1cc27ad33e9e0fc5adc775f1092fd9a404723df3
GabrielPertile <gabriel-pertile@hotmail.com>
2023-07-03 11:13:14 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,7 @@ public class Program {
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		
+
 		Seller seller = new Seller(1, "Gabriel Pertile", "gabriel@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(obj);
 
